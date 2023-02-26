e68f960c54ac64c644f73f6ef3d01bdb2dc329e3
AnnRefr <Ann_malaza@ukr.net>
2023-02-26 20:44:18 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import Notiflix from 'notiflix';
 import {fetchPhotos} from "./js/axios";
 
@@ -15,7 +16,13 @@ ref.loadElem.style.display = 'none';
 let page = 1;
 let total = 0;
 
-const simpleligthbox = new SimpleLightbox('.gallery a', { loop: false });
+let simplelightbox = new SimpleLightbox('.js-gallery', { 
+    captions: true,
+    captionsData: "alt",
+    captionSelector: "img",
+    captionDelay: 250,
+    captionPosition: "bottom"});
+
 
 ref.butElem.addEventListener("click", onClickButton);
 ref.loadElem.addEventListener("click", onLoad)
@@ -31,51 +38,45 @@ function onClickButton(e) {
         ref.loadElem.style.display = 'none';
         return
     }
-    console.log(ref.inpElem.value);
     fetchPhotos(ref.inpElem.value)
-        .then(data => {
-            total = 0;
-            console.log(data);
-            total += data.hits.length;
-            console.log(data.totalHits);
-            if (data.total == 0) {
-                Notiflix.Notify.success('"Sorry, there are no images matching your search query. Please try again."');
-                ref.gallery.innerHTML = "";
-            }
+    .then(data => {
+        total = 0;
+        total += data.hits.length;
+        if (data.total == 0) {
+            Notiflix.Notify.success('"Sorry, there are no images matching your search query. Please try again."');
             ref.gallery.innerHTML = "";
-            ref.gallery.insertAdjacentHTML("beforeend", createMarkupInfo(data.hits));
-            if (total === Number(data.totalHits)) {
-                Notiflix.Notify.success("We're sorry, but you've reached the end of search results.");
-                ref.loadElem.style.display = 'none';
-            } else {
-                ref.loadElem.style.display = 'block';    
-            }
-        })
-        .catch(err => {
-            console.log(err);
-            Notiflix.Notify.success('Sol lucet omnibus');
-            ref.gallery.innerHTML = "";
-        }).then(() => simpleligthbox.refresh());
+        }
+        ref.gallery.innerHTML = "";
+        ref.gallery.insertAdjacentHTML("beforeend", createMarkupInfo(data.hits));
+    })
+    .catch(err => {
+        Notiflix.Notify.success('Sol lucet omnibus');
+        ref.gallery.innerHTML = "";
+    })
 }
 
 function createMarkupInfo(arr) {
     return arr.map(({ webformatURL, tags, likes, views, comments, downloads }) => `
     <div class="photo-card">
-        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-        <div class="info">
-            <p class="info-item">
-                <b>Likes </b>${likes}
-            </p>
-            <p class="info-item">
-                <b>Views </b>${views}
-            </p>
-            <p class="info-item">
-                <b>Comments </b>${comments}
-            </p>
-            <p class="info-item">
-                <b>Downloads </b>${downloads}
-            </p>
-        </div>
+    <img src="${webformatURL}" alt="${tags}" loading="lazy" width="300" height= "200"/>
+    <div class="info">
+    <p class="info-item">
+    <b>Likes
+    </b>${likes}
+    </p>
+    <p class="info-item">
+    <b>Views
+    </b>${views}
+    </p>
+    <p class="info-item">
+    <b>Comments
+    </b>${comments}
+    </p>
+    <p class="info-item">
+    <b>Downloads
+    </b>${downloads}
+    </p>
+    </div>
     </div>`).join(" ");
 }
 
@@ -84,26 +85,16 @@ function onLoad() {
     ref.loadElem.style.display = 'none';
     fetchPhotos(ref.inpElem.value, page)
         .then(data => {
-            console.log(data);
             total += data.hits.length;
             if (data.total == 0) {
-                Notiflix.Notify.success('"Sorry, there are no images matching your search query. Please try again."');
+                Notiflix.Notify.success("Sorry, there are no images matching your search query. Please try again.");
                 ref.gallery.innerHTML = "";
-            }
-            ref.gallery.insertAdjacentHTML("beforeend", createMarkupInfo(data.hits));
-            //console.log(createMarkupInfo(data.hits));
-            if (total === Number(data.totalHits)) {
-                ref.loadElem.style.display = 'none';
-                Notiflix.Notify.success("We're sorry, but you've reached the end of search results.");
-            } else {
-                ref.loadElem.style.display = 'block';
             }
         })
         .catch(err => {
-            console.log(err);
             Notiflix.Notify.success('Sol lucet omnibus');
             ref.gallery.innerHTML = "";
-        }).then(() => simpleligthbox.refresh());
+        })
 }
 
 
